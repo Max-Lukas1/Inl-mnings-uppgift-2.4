b09ea2ec40b481b4fd29bd86ce87192ac4f8a21c
majo06002 <Max-Lukas@outlook.com>
2023-09-20 10:32:12 +0200

    Add project files.

---
+++ Inlämnings uppgift 2.4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace inlämningsuppgift_2
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur hög är din lön användare1?");
+            string använder1=Console.ReadLine();
+            int använders1 = int.Parse(använder1);
+            Console.WriteLine("Hur hög är din lön användare 2?");
+            string använder2 = Console.ReadLine();
+            int använders2 = int.Parse(använder2);
+
+            Console.WriteLine("Hur hög är din lön användare 3?");
+            string använder3 = Console.ReadLine();
+            int använders3 = int.Parse(använder3);
+            int summan = (använders1 + använders2 + använders3) / 3;
+            Console.WriteLine("medellönen är "+summan+ "kr");
+        }
+    }
+}
+
